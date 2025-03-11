898475c437764dd9e3d09e69443c2b77c2380461
charmful0x <charmful0x@gmail.com>
2025-03-11 22:56:59 +0100

    chore: upgrade bundler dep

--- src/utils/bundles.rs
+++ src/utils/bundles.rs
@@ -1,8 +1,9 @@
 use anyhow::Error;
 use bundler::utils::core::bundle::Bundle;
 use bundler::utils::core::tx_envelope_writer::TxEnvelopeWrapper;
+use bundler::utils::constants::ADDRESS_BABE1;
 
 pub async fn retrieve_bundle_envelopes(txid: String) -> Result<Vec<TxEnvelopeWrapper>, Error> {
-    let bundle = Bundle::retrieve_envelopes(txid).await?;
+    let bundle = Bundle::retrieve_envelopes(txid, ADDRESS_BABE1).await?;
     Ok(bundle.envelopes)
 }
